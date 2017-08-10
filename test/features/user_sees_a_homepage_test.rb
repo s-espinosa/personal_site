cda5c219312426f1f2c7aeb314c37f508c9e4c49
Sal Espinosa <sespinos@gmail.com>
2017-08-10 12:19:33 -0600

    Add views, css, and more tests

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,8 +4,21 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    save_and_open_page
     assert page.has_content?("Welcome!")
+    assert_equal 200, page.status_code
+  end
+
+  def test_user_can_see_error_page
+    visit '/does-not-exist'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
+
+  def test_user_sees_an_about_page
+    visit '/about'
+
+    assert page.has_content?("About Me!")
     assert_equal 200, page.status_code
   end
 end
